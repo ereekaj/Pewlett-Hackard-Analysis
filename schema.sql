96ec127ee391c3901633b4734b7aca891c8e8ccf
ereekaj <52435827+ereekaj@users.noreply.github.com>
2022-02-01 14:16:51 -0700

    adding queries

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
 -- Creating tables for PH-EmployeeDB
+DROP TABLE departments, employees, dept_manager, salaries, dept_emp, titles CASCADE;
+
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -17,7 +19,7 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -41,8 +43,7 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no) 
 );
 
 CREATE TABLE titles(
@@ -50,10 +51,7 @@ CREATE TABLE titles(
 	title VARCHAR NOT NULL, 
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-	
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 SELECT * FROM departments;
-
